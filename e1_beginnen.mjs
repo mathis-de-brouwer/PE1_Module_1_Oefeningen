a2f91acd6df9f452eda27c39a0a8daec67d8fc0e
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-09-28 16:22:31 +0100

    ex 1 done

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,7 @@
 // Voeg hier je eigen code in
-
-
-
+let name = ('Mathis de Brouwer');
+let age = parseFloat(19);
+let favoriteGame = ('Minecraft')
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
+
+process.exit();
